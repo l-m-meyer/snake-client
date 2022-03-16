ceaddd8c932e53a43f9cb2aa7ded1c49fb56d4a0
Lisa Meyer <lisamermey@gmail.com>
2022-03-16 15:30:28 -0400

    created handleUserInput() and setupInput() to register an event listener to exit program

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 
 const { connect } = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = () => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+
 console.log('Connecting...');
 connect();
